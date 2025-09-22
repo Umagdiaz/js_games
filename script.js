78c6eaa6e50a571d52d318cfaf06d3a329480303
Umagdiaz <umabaligranado@gmail.com>
2025-09-22 01:26:35 -0300

    nav dropdown and margins

---
+++ script.js
@@ -0,0 +1,25 @@
+// menu de navegacion //
+let btn_mainMenu = document.querySelector("#btn_mainmenu");
+let mainMenu = document.querySelector("#main_menu");
+let btn_juegos = document.querySelector("#btn_juego");
+let list_juegos = document.querySelector(".list_juegos");
+
+mainMenu.style.display = "none";
+list_juegos.style.display = "none";
+
+btn_mainMenu.addEventListener("click", () => {
+  if (mainMenu.style.display === "none") {
+    mainMenu.style.display = "block";
+  } else {
+    mainMenu.style.display = "none";
+  }
+});
+
+
+btn_juegos.addEventListener("click", () => {
+  if (list_juegos.style.display === "none") {
+    list_juegos.style.display = "block";
+  } else {
+    list_juegos.style.display = "none";
+  }
+});
